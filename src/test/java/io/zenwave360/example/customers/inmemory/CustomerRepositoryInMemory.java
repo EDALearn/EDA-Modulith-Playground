b499bd508653d4db04585ee6c52652ed8f97db54
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2025-05-16 10:42:23 +0200

    removes unfinished tests

--- src/test/java/io/zenwave360/example/customers/inmemory/CustomerRepositoryInMemory.java
+++
@@ -1,8 +0,0 @@
-package io.zenwave360.example.customers.inmemory;
-
-import io.zenwave360.example.customers.CustomerRepository;
-import io.zenwave360.example.customers.model.*;
-
-public class CustomerRepositoryInMemory extends InMemoryMongodbRepository<Customer> implements CustomerRepository {
-
-}
--- src/test/java/io/zenwave360/example/restaurants/infrastructure/mongodb/inmemory/MenuItemRepositoryInMemory.java
+++
@@ -1,8 +0,0 @@
-package io.zenwave360.example.restaurants.infrastructure.mongodb.inmemory;
-
-import io.zenwave360.example.restaurants.core.domain.*;
-import io.zenwave360.example.restaurants.core.outbound.mongodb.MenuItemRepository;
-
-public class MenuItemRepositoryInMemory extends InMemoryMongodbRepository<MenuItem> implements MenuItemRepository {
-
-}
